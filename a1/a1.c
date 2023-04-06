3a27e9b56c6cd15f7b379d9a515e7adff8178ddd
Altyn Charyyeva <altyncharyyeva1@gmail.com>
2023-04-06 23:58:38 +0300

    completed the parse part

--- a1/a1.c
+++ a1/a1.c
@@ -5,7 +5,120 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <ctype.h>
+#include <fcntl.h>
+
+
+struct section{
+  char sect_name[18];
+  unsigned int sect_type;
+  unsigned int sect_size;
+}secton;
+
+int parse_file(const char* filepth){
+    
+    // variabile de validare
+    
+      int ver_magic;
+      int ver_version = 0;
+      int ver_nr_of_sections =0 ; 
+    
+    // variabile pentru citire
+    int fd=-1;
+    
+    fd = open(filepth, O_RDONLY);
+    if(fd == -1){
+       perror("Couldn't open the file\n");
+       return -1;
+    }
+    
+    char magic;
+    if(read(fd, &magic, 1)==-1){
+       perror("Error while reading magic\n");
+    }
+    
+    if(magic=='o'){
+       ver_magic=1;
+    }else{
+       printf("ERROR\nwrong magic\n");
+       return -1;
+    }
+    
+    lseek(fd, 2, SEEK_CUR);
+    // reading version
+    unsigned char version;
+    if(read(fd, &version, 1)==-1){
+       perror("Error while reading version\n");
+    } 
+    int version_value=(int)version;
+    if(version_value>=103 && version_value<=133){
+       ver_version=1;
+      // printf("version=%d\n", version_value);
+    }else{
+       printf("ERROR\nwrong version");
+       return -1;
+    }
+    
+    
+    unsigned char no_of_sections;
+    if(read(fd, &no_of_sections, 1)==-1){
+       perror("Error while reading nr_of_sections\n");
+    } 
+    int nr_sections=(int)no_of_sections;
+    if(nr_sections>=7 && nr_sections<=19){
+       ver_nr_of_sections=1;
+       //printf("nr_sections=%d\n", nr_sections);
+    }else{
+       printf("ERROR\nwrong sect_nr\n");
+       return -1;
+    }
+    
+    
+    struct section* sections = (struct section*)malloc(nr_sections*sizeof(struct section));
+    if(sections==NULL){
+      printf("Couldnt allocate the sectons\n");
+      return -1;
+    }
+    
+    int ver_sect_type = 0;
+    for(int i=0; i<nr_sections; i++){
+       if(read(fd, &sections[i].sect_name, 17)==-1){
+          printf("Couldnt read the sect_name\n");
+          return -1;
+       }
+       sections->sect_name[17]='\0';
+       if(read(fd, &sections[i].sect_type, 4)==-1){
+          printf("Couldnt read the sect_name\n");
+          return -1;
+       }
+       if(sections[i].sect_type==49 || sections[i].sect_type==60 || sections[i].sect_type==38 || sections[i].sect_type==87 || sections[i].sect_type==54 || sections[i].sect_type==55 ){
+         ver_sect_type++;
+       }else{
+          printf("ERROR\nwrong sect_types\n");
+       }
+       lseek(fd, 4, SEEK_CUR);
+       if(read(fd, &sections[i].sect_size, 4)==-1){
+          printf("Couldnt read the sect_name\n");
+          return -1;
+       }
+       
+    }
+    
+    if(ver_magic==1 && ver_version==1 && ver_nr_of_sections==1 && ver_sect_type==nr_sections){
+       printf("SUCCESS\n");
+       printf("version=%d\n", version_value);
+       printf("nr_sections=%d\n", nr_sections);
+       for(int i=0; i<nr_sections; i++){
+          printf("section%d: %s %d %d\n", i+1, sections[i].sect_name, sections[i].sect_type, sections[i].sect_size);
+       }
+    } 
+    
+    free(sections);
+    
+    close(fd);   
+    
+    
+    return 0;
+}
 
 
 
@@ -104,6 +217,9 @@ int main(int argc, char* argv[]){
             if(strcmp(argv[i], "variant")==0){
                printf("25195\n");
             }
+            if(strcmp(argv[i], "parse")==0){
+               i++;
+            }
             if(strcmp(argv[i], "list")==0){
                i++;
            //    printf("i at list: %d\n", i);
@@ -133,14 +249,14 @@ int main(int argc, char* argv[]){
                   recursive=1;
                   i++;
              //     printf("i at recursive: %d\n", i);
-               }
-               if(i<argc && strncmp(argv[i], "path=", 5)==0){
+                }
+             }
+             if(i<argc && strncmp(argv[i], "path=", 5)==0){
                  dir_path=argv[i]+5;
                 // printf("i at path= %d\n", i);
              }
-          }
        }
-       if(strcmp(argv[1], "variant")!=0){
+       if(strcmp(argv[1], "variant")!=0 && strcmp(argv[1], "parse")!=0){
            if(dir_path==NULL){
            printf("ERROR: invalid directory path\n");
            return -1;
@@ -149,6 +265,14 @@ int main(int argc, char* argv[]){
            printf("SUCCESS\n");
            listDir(dir_path, recursive, value, size_filter, name_filter, str);
            
+       }
+       if(strcmp(argv[1], "parse")==0){
+          if(dir_path==NULL){
+            printf("ERROR: invalid directory path(in parse)\n");
+            return -1;
+          }
+          //printf("SUCCESS\n");
+          parse_file(dir_path);
        }
    }
    return 0;
