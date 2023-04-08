4f30a7c629353ecfe5ed9459d79943e986c94154
Altyn Charyyeva <altyncharyyeva1@gmail.com>
2023-04-08 20:13:36 +0300

    did the extract part

--- a1/a1.c
+++ a1/a1.c
@@ -8,21 +8,244 @@
 #include <fcntl.h>
 
 
-struct section{
+struct section_hd{
   char sect_name[18];
   unsigned int sect_type;
   unsigned int sect_size;
-}secton;
+}secton_hd;
+
+
+
+void free_matrix(char** matrix, unsigned int rows)
+{
+    for (unsigned int i = 0; i < rows; i++)
+    {
+        free(matrix[i]);
+    }
+
+    free(matrix);
+}
+
+
+int extract_content(const char* filepath, int sect_nr, int line_nr){
+    
+    // variabile de verificare daca fisierul e SF file sau nu
+    int ver_magic=0;
+    int ver_version = 0;
+    int ver_nr_of_sections = 0 ; 
+    
+    int fd=-1;
+    
+    fd = open(filepath, O_RDONLY);
+    if(fd == -1){
+       perror("Couldn't open the file\n");
+       return -1;
+    }
+    
+    // citesc magic si il verific
+    char magic;
+    if(read(fd, &magic, 1)==-1){
+       perror("Error while reading magic\n");
+    }
+    if(magic=='o'){
+       ver_magic=1;
+    }else{
+       printf("ERROR\ninvalid file\n");
+       return -1;
+    }
+    
+    
+    lseek(fd, 2, SEEK_CUR);  // sarim peste header_size cu 2 bytes
+    // citesc version si il verific
+    unsigned char version;
+    if(read(fd, &version, 1)==-1){
+       perror("Error while reading version\n");
+    } 
+    int version_value=(int)version;
+    if(version_value>=103 && version_value<=133){
+       ver_version=1;
+    }else{
+       printf("ERROR\ninvalid file\n");
+       return -1;
+    }
+    
+    
+    // citesc no_of_versions si il verific
+    unsigned char no_of_sections;
+    if(read(fd, &no_of_sections, 1)==-1){
+       perror("Error while reading nr_of_sections\n");
+    } 
+    int nr_sections=(int)no_of_sections;
+    if(nr_sections>=7 && nr_sections<=19){
+       ver_nr_of_sections=1;
+    }else{
+       printf("ERROR\ninvalid file\n");
+       return -1;
+    }
+    
+    // citesc sect_type-urile si le verific
+    int ver_sect_type = 0;
+    unsigned int sect_type;
+    for(int i=0; i<nr_sections; i++){
+       lseek(fd, 17, SEEK_CUR);
+       if(read(fd, &sect_type, 4)==-1){
+          printf("Couldnt read the sect_type\n");
+          return -1;
+       }
+       if(sect_type==49 || sect_type==60 || sect_type==38 || sect_type==87 || sect_type==54 || sect_type==55 ){
+         ver_sect_type++;
+       }else{
+          printf("ERROR\ninvalid file\n");
+       }
+       lseek(fd, 8, SEEK_CUR);
+       
+    }
+    
+    // verific daca fisierul e SF file si daca da, pun cursorul la inceputul fisierului si caut section-ul dat
+    unsigned int sect_offset=0;
+    unsigned int sect_size=0;
+    if(ver_magic==1 && ver_version==1 && ver_nr_of_sections==1 && ver_sect_type==nr_sections){
+        lseek(fd, 5, SEEK_SET);
+        for(int i=1; i<=nr_sections; i++){
+           if(sect_nr==i){
+              lseek(fd, 17, SEEK_CUR);  // sarim peste sect_name
+              lseek(fd, 4, SEEK_CUR);   // sarim peste sect_type
+              if(read(fd, &sect_offset, 4)==-1){  //citim sect_offset
+                  printf("Couldnt read the sect_offset\n");
+                  return -1;
+              }
+              if(read(fd, &sect_size, 4)==-1){
+                 printf("Couldnt read sect_size\n");
+                 return -1;
+              }
+              break;
+           }
+           lseek(fd, 17, SEEK_CUR);
+           lseek(fd, 12, SEEK_CUR);
+           
+        }
+        
+
+        lseek(fd, sect_offset, SEEK_SET);
+
+    
+    }
+    
+    // am gasit section-ul si citesc linii-le separat in array of strings si maresc dimensiunea cand citesc 
+    unsigned int nr_linie=0;
+    unsigned int nr_linie_max=1;
+    
+    unsigned int nr_chars=0;
+    unsigned int nr_chars_max=2;
+    
+    unsigned int size_checker=0;
+    
+    char buffer[512]; 
+    
+    char** section = (char**)malloc(nr_linie_max*sizeof(char*));
+    if(section==NULL){
+       printf("Couldnt allocate the memory\n");
+       return -1;
+    }
+    
+    section[nr_linie]=(char*)malloc(nr_chars_max*sizeof(char));
+    if(section[nr_linie]==NULL){
+      printf("Couldnt allocate the section line\n");
+      free_matrix(section, nr_linie);
+      return -1;
+    }
+    
+ 
+    for(unsigned int i=0; i<sect_size;){
+       if(read(fd, &buffer, sizeof(buffer))==-1){ // citesc un group de bytes si le verific daca e sfarsitul liniei sau nu   
+         printf("Error reading buffer\n");        // cand le pun in tablou de string-uri numit section
+         free_matrix(section, nr_linie);
+         return -1;
+       }
+       for(unsigned int j=0; j<sizeof(buffer); j++){
+          if(buffer[j]=='\n' && j>0 && buffer[j-1]=='\r'){  // am ajuns la sfarsitul liniei
+            section[nr_linie][nr_chars]='\0';
+            // reinitializam varibile de masura
+            nr_chars_max=2;
+            nr_chars=0;
+            if(nr_linie+1>=nr_linie_max){  
+              char** aux = realloc(section, 2*nr_linie_max*sizeof(char*));
+              if(aux==NULL){
+                printf("Couldnt do realloc in lines\n");
+                free_matrix(section, nr_linie);
+                return -1;
+              }
+              section=aux;
+              nr_linie_max*=2;
+            }
+            section[nr_linie+1] = (char*)malloc(nr_chars_max+1);
+            if(section[nr_linie+1]==NULL){
+               printf("Couldnt do malloc for the next line\n");
+               free_matrix(section, nr_linie);
+               return -1;
+            }
+            nr_linie++;
+            continue;
+          }
+          if(nr_chars>=nr_chars_max){
+             char* aux = realloc(section[nr_linie], 2*nr_chars_max+1);
+             if(aux==NULL){
+               printf("Couldnt do realloc\n");
+               free_matrix(section, nr_linie);
+               return -1;
+             }
+             section[nr_linie]=aux;
+             nr_chars_max*=2;
+          }
+          if(size_checker==sect_size){
+            break;
+          }
+          section[nr_linie][nr_chars]=buffer[j];
+          nr_chars++;
+          size_checker++;
+          
+       }
+       if(size_checker==sect_size){
+         break;
+       }
+       i+=(sizeof(buffer));
+    }
+    
+
+     // Add any remaining data to the section matrix
+        if(nr_chars > 0) {
+            section[nr_linie][nr_chars] = '\0';
+        }
+
+
+     close(fd);
+
+    // citesc linii in ordinea invers
+    unsigned int k=nr_linie-1;
+    for(unsigned int i=0; i<nr_linie; i++){
+      if(line_nr==i+1){
+       printf("SUCCESS\n");
+       printf("%s\n", section[k+1]); 
+       break; 
+      }
+      k--;
+    }
+    
+    free_matrix(section, nr_linie);
+
+    return 0;
+    
+}
 
 int parse_file(const char* filepth){
     
     // variabile de validare
     
-      int ver_magic;
-      int ver_version = 0;
-      int ver_nr_of_sections =0 ; 
+    int ver_magic = 0;
+    int ver_version = 0;
+    int ver_nr_of_sections = 0 ; 
     
-    // variabile pentru citire
+
     int fd=-1;
     
     fd = open(filepth, O_RDONLY);
@@ -44,7 +267,6 @@ int parse_file(const char* filepth){
     }
     
     lseek(fd, 2, SEEK_CUR);
-    // reading version
     unsigned char version;
     if(read(fd, &version, 1)==-1){
        perror("Error while reading version\n");
@@ -52,7 +274,6 @@ int parse_file(const char* filepth){
     int version_value=(int)version;
     if(version_value>=103 && version_value<=133){
        ver_version=1;
-      // printf("version=%d\n", version_value);
     }else{
        printf("ERROR\nwrong version");
        return -1;
@@ -66,14 +287,13 @@ int parse_file(const char* filepth){
     int nr_sections=(int)no_of_sections;
     if(nr_sections>=7 && nr_sections<=19){
        ver_nr_of_sections=1;
-       //printf("nr_sections=%d\n", nr_sections);
     }else{
        printf("ERROR\nwrong sect_nr\n");
        return -1;
     }
     
-    
-    struct section* sections = (struct section*)malloc(nr_sections*sizeof(struct section));
+    // pun in section headers header_name-uri, sect_type-uri, sect_offset-uri si sect_size-uri
+    struct section_hd* sections = (struct section_hd*)malloc(nr_sections*sizeof(struct section_hd));
     if(sections==NULL){
       printf("Couldnt allocate the sectons\n");
       return -1;
@@ -210,9 +430,10 @@ int main(int argc, char* argv[]){
     int size_filter=0;
     int name_filter=0;
     char* str=NULL;
+    int sect_nr=0;
+    int line_nr=0;
   
        if(argc>=2){
-         //printf("argc: %d\n", argc);
          for(i=1; i<argc; i++){
             if(strcmp(argv[i], "variant")==0){
                printf("25195\n");
@@ -220,48 +441,74 @@ int main(int argc, char* argv[]){
             if(strcmp(argv[i], "parse")==0){
                i++;
             }
-            if(strcmp(argv[i], "list")==0){
+            if(strcmp(argv[i], "findall")==0){
                i++;
-           //    printf("i at list: %d\n", i);
-               if(i<argc && strcmp(argv[i], "recursive")==0){
-                  recursive=1;
-                  i++;
-             //     printf("i at recursive: %d\n", i);
-               }
-               if(i<argc && strncmp(argv[i], "size_smaller=", 13)==0){
-                 size_filter=1;
-                 value=atoll((argv[i]+13));
+            }
+            if(strcmp(argv[i], "extract")==0){
                  i++;
-               //  printf("i at size_smaller: %d\n", i);
-               }
-               if(i<argc && strcmp(argv[i], "recursive")==0){
-                  recursive=1;
-                  i++;
-             //     printf("i at recursive: %d\n", i);
-               }
-               if(i<argc && strncmp(argv[i], "name_ends_with=", 15)==0){
-                 name_filter=1;
-                 str=argv[i]+15;
+                 if(i<argc && strncmp(argv[i], "path=", 5)==0){
+                     dir_path=argv[i]+5;
+                     i++;
+                 }
+                 if(i<argc && strncmp(argv[i], "section=", 8)==0){
+                     sect_nr=atoi(argv[i]+8);
+                     i++;
+                 }
+                 if(i<argc && strncmp(argv[i], "line=", 5)==0){
+                     line_nr=atoi(argv[i]+5);
+
+                 }
+              
+            }
+            if(strcmp(argv[i], "list")==0){
                  i++;
-                 //printf("i at name_ends_with: %d\n", i);
-               }
-               if(i<argc && strcmp(argv[i], "recursive")==0){
-                  recursive=1;
-                  i++;
-             //     printf("i at recursive: %d\n", i);
-                }
+                 if(i<argc && strcmp(argv[i], "recursive")==0){
+                     recursive=1;
+                     i++;
+                 }
+                 if(i<argc && strncmp(argv[i], "size_smaller=", 13)==0){
+                      size_filter=1;
+                      value=atoll((argv[i]+13));
+                      i++;
+                 }
+                 if(i<argc && strcmp(argv[i], "recursive")==0){
+                     recursive=1;
+                     i++;
+                  }
+                  if(i<argc && strncmp(argv[i], "name_ends_with=", 15)==0){
+                      name_filter=1;
+                      str=argv[i]+15;
+                      i++;
+                  }
+                  if(i<argc && strcmp(argv[i], "recursive")==0){
+                      recursive=1;
+                      i++;
+                  }
              }
              if(i<argc && strncmp(argv[i], "path=", 5)==0){
                  dir_path=argv[i]+5;
-                // printf("i at path= %d\n", i);
              }
        }
-       if(strcmp(argv[1], "variant")!=0 && strcmp(argv[1], "parse")!=0){
+       if(strcmp(argv[1], "extract")==0){
+          if(dir_path==NULL){
+            printf("ERROR: invalid directory path\n");
+            return -1;
+          }
+          extract_content(dir_path, sect_nr, line_nr);
+       }
+       if(strcmp(argv[1], "findall")==0){
+         if(dir_path==NULL){
+            printf("ERROR: invalid directory path\n");
+            return -1;
+         }
+          printf("SUCCESS\n");
+          //findall(dir_path);
+       }
+       if(strcmp(argv[1], "list")==0){
            if(dir_path==NULL){
            printf("ERROR: invalid directory path\n");
            return -1;
            }
-           //printf("dir_path: %s\nrecursive: %d\nvalue: %ld\nsize_filter: %d\nname_filter: %d\nstr: %s\n", dir_path, recursive, value, size_filter, name_filter, str);
            printf("SUCCESS\n");
            listDir(dir_path, recursive, value, size_filter, name_filter, str);
            
@@ -271,7 +518,6 @@ int main(int argc, char* argv[]){
             printf("ERROR: invalid directory path(in parse)\n");
             return -1;
           }
-          //printf("SUCCESS\n");
           parse_file(dir_path);
        }
    }
